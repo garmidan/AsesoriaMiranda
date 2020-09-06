fb85a7128bcaa613ba37e1c2b5c8eda4c7da7dee
Jairmt <37247987+Jairmt@users.noreply.github.com>
2020-09-06 16:41:04 -0500

    Cambios estilo
    Estilo Login

--- AsesoriaMirandaFrontend/src/app/app-routing.module.ts
+++ AsesoriaMirandaFrontend/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './templates/login/login.component';
+import { HomeComponent } from './templates/home/home.component';
+import { AcercaComponent } from './templates/acerca/acerca.component';
+
+
 
 
 const routes: Routes = [
-  {path: 'login', component:LoginComponent},
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'acerca', component: AcercaComponent },
 ];
 
 @NgModule({
--- AsesoriaMirandaFrontend/src/app/app.module.ts
+++ AsesoriaMirandaFrontend/src/app/app.module.ts
@@ -5,11 +5,15 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './templates/login/login.component';
+import { HomeComponent } from './templates/home/home.component';
+import { AcercaComponent } from './templates/acerca/acerca.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    AcercaComponent,
   ],
   imports: [
     BrowserModule,
